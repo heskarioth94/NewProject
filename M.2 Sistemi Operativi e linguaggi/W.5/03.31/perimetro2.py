0e960a4b48ca0731159e14a1878ef9efc1e44b19
heskarioth94 <volterra.andrea94@gmail.com>
2023-03-31 19:59:59 +0200

    modifica codice

--- M.2 Sistemi Operativi e linguaggi/W.5/03.31/perimetro2.py
+++ M.2 Sistemi Operativi e linguaggi/W.5/03.31/perimetro2.py
@@ -1,41 +1,40 @@
 import math
 
-while True:
-    print("Scegli la figura di cui calcolare il perimetro:\n A. Quadrato\n B. Cerchio\n C. Rettangolo\n")
+gioco = True
+while gioco:
+    print("Scegli la figura di cui calcolare il perimetro:\n A. Quadrato\n B. Cerchio\n C. Rettangolo\n Q. Chiudi il programma")
     x = input ("Digita la tua selezione: ")
  
     if x == "A" or x == "a" :
-        while True:
+        while gioco:
             try:
                 l = int (input ("Inserisci il lato: "))
                 if l<=0:
                     print("Il valore deve essere maggiore di zero, riprova.")
                 else:    
                     perimetro1 = int (l*4)
-                    print ("Il perimetro del quadrato è: " )
-                    print (perimetro1)
+                    print (f"Il perimetro del quadrato è:{perimetro1} " )
                     break
             except ValueError:
                 print("Input non valido, per favore inserisci un numero intero.")
         break    
  
     elif x == "B" or x == "b":
-        while True:
+        while gioco:
             try:
                 r = int (input ("Inserisci il raggio: "))
                 if r<=0:
                     print("Il valore deve essere maggiore di zero, riprova.")
                 else:
                     circonferenza = r * 2 * math.pi
-                    print ("La circonferenza del cerchio è: ")
-                    print (circonferenza)
+                    print (f"La circonferenza del cerchio è: {circonferenza} ")
                     break
             except ValueError:
                 print("Input non valido, per favore inserisci un numero intero.")
         break    
  
     elif x == "C" or x == "c":
-        while True:
+        while gioco:
             try:
                 b = int (input ("Inserisci la base: "))
                 h = int (input("Inserisci l'altezza: "))
@@ -43,16 +42,19 @@ while True:
                     print("Il valore deve essere maggiore di zero, riprova.")
                 else:
                     perimetro2 = b * 2 + h * 2
-                    print ("Il perimetro del rettangolo è: " )
-                    print (perimetro2)
+                    print (f"Il perimetro del rettangolo è: {perimetro2} " )
+
                     break
             except ValueError:
                 print("Input non valido, per favore inserisci un numero intero.")
         break        
-    
-    
+
+    elif x == "Q" or x == "q":
+        gioco = False
+        print ("Hai terminato il programma")
+        break
  
     else:
         print ("Scelta non valida, per favore riprova")
-        
+       
     
