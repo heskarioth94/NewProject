895c9fd67a749b0896026b7d0bee81652f5e1bba
heskarioth94 <volterra.andrea94@gmail.com>
2023-04-04 20:54:13 +0200

    aggiunge v3

--- M.2 Sistemi Operativi e linguaggi/W.6/04.04/password_gen.py
+++ M.2 Sistemi Operativi e linguaggi/W.6/04.04/password_gen.py
@@ -1,22 +1,31 @@
 import string
 import random
 
-
+def randomizer8():
+    number_of_strings = 1
+    length_of_string = 8
+    for x in range(number_of_strings):
+        print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
+        
+def randomizer20():
+    number_of_strings = 1
+    length_of_string = 20
+    for x in range(number_of_strings):
+        print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
+        
+        
 def password_gen():
-
+    
     scelta = input("Questo programma serve per generare password!\n>>> 1. per una password di 8 caratteri\n>>> 2. per una password di 20 caratteri\n>>> Q. Chiudi il programma\nEffettua una scelta:\n>>> ")
 
     if scelta == '1':
-        number_of_strings = 1
         length_of_string = 8
-        for x in range(number_of_strings):
-            print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
+        print("La password generata è: ")
+        randomizer8()
 
     elif scelta == '2':
-        number_of_strings = 1
-        length_of_string = 20
-        for x in range(number_of_strings):
-            print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
+        print("La password generata è: ")
+        randomizer20()
 
     elif scelta == 'q' or scelta == 'Q':
         print("Hai terminato il programma")
