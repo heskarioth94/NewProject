6cad6d8f556f9ecc0b5445457c8f8bc545c22835
heskarioth94 <volterra.andrea94@gmail.com>
2023-04-04 19:35:04 +0200

    aggiunge funzione liste

---
+++ M.2 Sistemi Operativi e linguaggi/W.6/nuovalista.py
@@ -0,0 +1,10 @@
+#Funzione per creare una lista che conti i caratteri delle parola in un'altra lista
+
+def lista ():
+    list_a = ['meteorite', 'gesso', 'dichiarare', 'quartiere', 'lucernario', 'propaganda', 'bermuda']
+    print ("La lista A è: ", list_a)
+    list_b = [len(list_a[0]), len(list_a[1]), len(list_a[2]), len(list_a[3]), len(list_a[4]), len(list_a[5]), len(list_a[6])]
+    print ("La lista B è: ", list_b)
+
+
+lista()
