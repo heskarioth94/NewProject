1c3d52c086ac6c67e43f1378c8aa3ea180450157
heskarioth94 <volterra.andrea94@gmail.com>
2023-04-04 20:27:36 +0200

    mette v2

--- M.2 Sistemi Operativi e linguaggi/W.6/04.04/password_gen.py
+++ M.2 Sistemi Operativi e linguaggi/W.6/04.04/password_gen.py
@@ -4,19 +4,23 @@ import random
 
 def password_gen():
 
-    scelta = int(input("Questo programma serve per generare password!\n>>> 1 per una password di 8 caratteri\n>>> 2 per una password di 20 caratteri\nEffettua una scelta:\n>>>"))
+    scelta = input("Questo programma serve per generare password!\n>>> 1. per una password di 8 caratteri\n>>> 2. per una password di 20 caratteri\n>>> Q. Chiudi il programma\nEffettua una scelta:\n>>> ")
 
-    if scelta == 1:
+    if scelta == '1':
         number_of_strings = 1
         length_of_string = 8
         for x in range(number_of_strings):
             print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
 
-    elif scelta == 2:
+    elif scelta == '2':
         number_of_strings = 1
         length_of_string = 20
         for x in range(number_of_strings):
             print(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string)))
+
+    elif scelta == 'q' or scelta == 'Q':
+        print("Hai terminato il programma")
+        quit()
 
     else:
         print("Scelta non valida")
